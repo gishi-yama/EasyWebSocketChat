78fb4e32a692b5fb1d17be296c9f2bf8ffe4f857
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2015-07-23 14:40:34 +0900

    suppert jpg/png

--- src/java/endpoint/Chat.java
+++ src/java/endpoint/Chat.java
@@ -1,6 +1,9 @@
 package endpoint;
 
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.stream.Stream;
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
@@ -16,12 +19,11 @@ public class Chat {
   }
 
   @OnMessage
-  public void processUpload(ByteBuffer msg, boolean last, Session session) {
-    System.out.println("Binary Data");
-
-    while (msg.hasRemaining()) {
-      
-    }
-
+  public void processUpload(ByteBuffer bytes, boolean last, Session onSession) {
+    System.out.println("Binary");
+//    onMessage(Base64.encode(bytes.array()), onSession);
+    onSession.getOpenSessions()
+            .stream()
+            .forEach(s -> s.getAsyncRemote().sendBinary(bytes));
   }
 }
